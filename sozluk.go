87f9ee7288dbee3bfd9284bcecd6803ab96251e4
erayarslan <relfishere@gmail.com>
2017-09-07 09:36:15 +0300

    makefile added

--- sozluk.go
+++ sozluk.go
@@ -14,11 +14,13 @@ import (
 
 var token string
 
+// CheckResponse :
 type CheckResponse struct {
 	Success bool       `json:"success"`
 	Data    *CheckData `json:"data"`
 }
 
+// CheckData :
 type CheckData struct {
 	IsAlive bool   `json: "isAlive"`
 	User_Id string `json: "user_id, omitempty"`
@@ -26,36 +28,43 @@ type CheckData struct {
 	Unread  int    `json: "unread`
 }
 
+// SearchResponse :
 type SearchResponse struct {
 	Success bool        `json:"success"`
 	Data    *SearchData `json:"data"`
 }
 
+// SearchData :
 type SearchData struct {
 	Topics []SearchDataTopics `json:"topics"`
 	Users  []SearchDataUsers  `json:"users"`
 }
 
+// SearchDataTopics :
 type SearchDataTopics struct {
 	ID    int    `json:"id"`
 	Slug  string `json:"slug"`
 	Title string `json:"title"`
 }
 
+// SearchDataUsers :
 type SearchDataUsers struct {
 	Username string `json:"username"`
 	Slug     string `json:"slug"`
 }
 
+// EntryCreateResponse :
 type EntryCreateResponse struct {
 	Success bool             `json:"success"`
 	Data    *EntryCreateData `json:"data"`
 }
 
+// EntryCreateData :
 type EntryCreateData struct {
 	ID int `json:"id"`
 }
 
+// TopicCreateResponse :
 type TopicCreateResponse struct {
 	Success bool `json:"success"`
 	ID      int  `json:"entry_id"`
